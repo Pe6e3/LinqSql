40023cdabe7ce5c4cb7ede81274e2c68f5c21bc7
Pe6e3 <xpe6e3@gmail.com>
2023-04-03 19:12:19 +0600

    Add project files.

---
+++ LinqSql/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinqSql.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
